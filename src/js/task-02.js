c57b57d2f635b3930584d58ae6df16ccad36faf1
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2023-07-15 21:13:54 +0200

    js6

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const items = ingredients.map((ingredient) => {
+  const liItems = document.createElement("li");
+  liItems.textContent = ingredient;
+  liItems.classList.add("item");
+
+  return liItems;
+});
+list.append(...items);
+
+console.log(items);
